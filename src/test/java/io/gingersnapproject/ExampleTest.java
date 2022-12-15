85fb8ee25ab5f6533356e377a944b32ae86f3e54
Ryan Emerson <remerson@redhat.com>
2022-12-15 15:44:21 +0000

    Give it a go

---
+++ src/test/java/io/gingersnapproject/ExampleTest.java
@@ -0,0 +1,10 @@
+package io.gingersnapproject;
+
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+   @Test
+   public void helloTests() {
+      System.out.println("Hello World");
+   }
+}
